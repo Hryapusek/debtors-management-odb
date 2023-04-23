9a2e5782a8ed7e5d140b674c0d20a838405a1ae4
Александр Абраамян <samvelcc25@outlook.com>
2023-04-23 12:23:33 +0300

    new: added common functions.
    Created functions that work with shared_ptr pointing to an object in table.
    This was made due to functions had a lot in common. Code duplication is not good.

--- db_api.cpp
+++ db_api.cpp
@@ -13,6 +13,42 @@ using namespace odb::core;
 
 namespace api
 {
+  void removeDebtor_(std::shared_ptr<Debtor> debtor)
+  {
+    auto &db = DbConnector::db;
+    db->erase(debtor);
+  }
+
+  void addValue_(std::shared_ptr<Debtor> debtor, int val, const std::string &description)
+  {
+    auto &db = DbConnector::db;
+    debtor->debt(debtor->debt() + val);
+    db->update(debtor);
+    Operation op(debtor, val, description);
+    db->persist< Operation >(op);
+  }
+
+  void rollback_(std::shared_ptr<Debtor> debtor)
+  {
+    auto &db = DbConnector::db;
+    const auto &operations = debtor->operations();
+    if (operations.empty())
+      throw api_exception("Can not rollback: no operations found");
+    std::shared_ptr< Operation > lastOperationPtr = operations.back().lock();
+    debtor->debt(debtor->debt() - lastOperationPtr->valueChange());
+    db->update(debtor);
+    db->erase< Operation >(lastOperationPtr);
+  }
+
+  std::vector< Operation > getOperations_(unsigned int id)
+  {
+    auto &db = DbConnector::db;
+    result< Operation > res(db->query< Operation >(query< Operation >::debtor == id));
+    std::vector< Operation > operations;
+    std::copy(res.begin(), res.end(), std::back_inserter(operations));
+    return operations;
+  }
+
   std::vector< Debtor > getDebtors()
   {
     auto &db = DbConnector::db;
@@ -32,20 +68,20 @@ namespace api
     transaction t(db->begin());
     auto res = db->query_one< Debtor >(query< Debtor >::name == name);
     if (res == nullptr)
-      return boost::optional< Debtor >();
+      return boost::none;
     Debtor d = *res;
     t.commit();
     return d;
   }
 
-  Debtor getDebtor(unsigned long id)
+  boost::optional< Debtor > getDebtor(unsigned long id)
   {
     auto &db = DbConnector::db;
     odb::session s;
     transaction t(db->begin());
     auto res = db->query_one< Debtor >(query< Debtor >::id == id);
     if (res == nullptr)
-      throw std::logic_error("Debtor::getDebtor(unsigned long id): User with id " + std::to_string(id) + " was not found");
+      return boost::none;
     Debtor d = *res;
     t.commit();
     return d;
@@ -67,8 +103,8 @@ namespace api
     transaction t(db->begin());
     auto debtor = db->query_one< Debtor >(query< Debtor >::name == name);
     if (debtor == nullptr)
-      throw std::logic_error("User with name " + name + " was not found");
-    db->erase(db->load< Debtor >(debtor->id()));
+      throw debtor_not_found("Debtor with name " + name + " was not found");
+    removeDebtor_(debtor);
     t.commit();
   }
 
@@ -77,7 +113,10 @@ namespace api
     auto &db = DbConnector::db;
     odb::session s;
     transaction t(db->begin());
-    db->erase(db->load< Debtor >(id));
+    auto debtor = db->query_one< Debtor >(query< Debtor >::id == id);
+    if (debtor == nullptr)
+      throw debtor_not_found("Debtor with id " + std::to_string(id) + " was not found");
+    removeDebtor_(debtor);
     t.commit();
   }
 
@@ -88,11 +127,20 @@ namespace api
     transaction t(db->begin());
     auto debtor = db->query_one< Debtor >(query< Debtor >::name == name);
     if (debtor == nullptr)
-      throw std::logic_error("User with name " + name + " was not found");
-    debtor->debt(debtor->debt() + val);
-    db->update(debtor);
-    Operation op(debtor, val, 0, description);
-    db->persist< Operation >(op);
+      throw debtor_not_found("Debtor with name " + name + " was not found");
+    addValue_(debtor, val, description);
+    t.commit();
+  }
+
+  void addValue(unsigned int id, int val, const std::string &description)
+  {
+    auto &db = DbConnector::db;
+    odb::session s;
+    transaction t(db->begin());
+    auto debtor = db->query_one< Debtor >(query< Debtor >::id == id);
+    if (debtor == nullptr)
+      throw debtor_not_found("Debtor with id " + std::to_string(id) + " was not found");
+    addValue_(debtor, val, description);
     t.commit();
   }
 
@@ -103,17 +151,20 @@ namespace api
     transaction t(db->begin());
     auto debtor = db->query_one< Debtor >(query< Debtor >::name == name);
     if (debtor == nullptr)
-      throw std::logic_error("User with name " + name + " was not found");
-    const auto &operations = debtor->operations();
-    if (operations.empty())
-      throw std::logic_error("Can not rollback: no operations found");
-    std::shared_ptr< Operation > lastOperationPtr = operations.back().lock();
-    if (!lastOperationPtr)
-      throw std::logic_error("rollback(const std::string &name): empty shared_ptr");
-    int valChange = lastOperationPtr->valueChange();
-    debtor->debt(debtor->debt() - valChange);
-    db->update(debtor);
-    db->erase< Operation >(lastOperationPtr);
+      throw debtor_not_found("Debtor with name " + name + " was not found");
+    rollback_(debtor);
+    t.commit();
+  }
+
+  void rollback(unsigned int id)
+  {
+    auto &db = DbConnector::db;
+    odb::session s;
+    transaction t(db->begin());
+    auto debtor = db->query_one< Debtor >(query< Debtor >::id == id);
+    if (debtor == nullptr)
+      throw debtor_not_found("Debtor with id " + std::to_string(id) + " was not found");
+    rollback_(debtor);
     t.commit();
   }
 
@@ -124,14 +175,22 @@ namespace api
     {
       auto res = getDebtor(name);
       if (!res)
-        throw std::logic_error("User with name " + name + " was not found");
+        throw debtor_not_found("Debtor with name " + name + " was not found");
       d = res.get();
     }
     odb::session s;
     transaction t(db->begin());
-    result< Operation > res(db->query< Operation >(query< Operation >::debtor == d.id()));
-    std::vector< Operation > operations;
-    std::copy(res.begin(), res.end(), std::back_inserter(operations));
+    std::vector< Operation > operations = getOperations_(d.id());
+    t.commit();
+    return operations;
+  }
+
+  std::vector< Operation > getOperations(unsigned int id)
+  {
+    auto &db = DbConnector::db;
+    odb::session s;
+    transaction t(db->begin());
+    std::vector< Operation > operations = getOperations_(id);
     t.commit();
     return operations;
   }
