566ea5450ca6fccbe62c09e8a382ac1768f803e1
Александр Абраамян <samvelcc25@outlook.com>
2023-04-23 11:59:03 +0300

    new: TokenStream now works with istream directly
    Since now TokenStream gets Database structs directly from istream.
    Previously we passed array of args, which was not so comfortable to
    extract structs from. Also this change was made because of change of
    input data format. Now Debtor is read like { id, "name", val} and
    Operation is read like {id, "name", valueChange, "description"}.
    In some cases id or "name" can be ommited.

--- token_stream.cpp
+++ token_stream.cpp
@@ -1,6 +1,6 @@
 #include "token_stream.hpp"
 #include <boost/regex.hpp>
-#include <iostream>
+#include "ioctl.hpp"
 
 namespace
 {
@@ -9,108 +9,215 @@ namespace
     boost::regex ex("[a-zA-Zа-яА-Я]{2,20}");
     return boost::regex_match(name, ex);
   }
-}
 
+  //Skips isspace(in.peek()) but new line
+  std::istream &skipWs(std::istream &in)
+  {
+    while (!in.fail() && !in.eof() && in.peek() != '\n' && isspace(in.peek()))
+      in.ignore(1);
+    return in;
+  }
 
-TokenStream::TokenStream(const std::vector< std::string > &args) :
-  args_(args),
-  it_(args.begin())
-{ }
-
-TokenStream::TokenStream(const std::vector< std::string > &args, std::vector< std::string >::const_iterator it) :
-  args_(args),
-  it_(it)
-{ }
-
-std::string TokenStream::readName()
-{
-  if (eof_ || fail_)
-  {
-    fail_ = true;
-    return std::string();
-  }
-  std::string name;
-  for (; it_ != args_.end(); ++it_)
-  {
-    if (!isName(*it_) && !isName(it_->substr(0, it_->size() - 1)))
-    {
-      fail_ = true;
-      return name;
-    }
-    if (it_->back() == '\\')
-    {
-      name += *it_ + " ";
-      name.erase(---- name.end());
-      continue;
-    }
-    else
-    {
-      name += *it_;
-      ++it_;
-      break;
-    }
-  }
-  if (name.empty())
-    fail_ = true;
-  else if (name.back() == ' ')
-    name.pop_back();
-  checkEof();
-  return name;
-}
-
-int TokenStream::readValue()
-{
-  if (eof_ || fail_)
-  {
-    fail_ = true;
-    return 0;
-  }
-  try
-  {
-    int res = std::stoi(*it_);
-    ++it_;
-    checkEof();
-    return res;
-  }
-  catch (const std::exception &e)
-  {
-    fail_ = true;
-    return 0;
-  }
-}
-
-Debtor TokenStream::readDebtor(bool valueExpected)
-{
-  if (eof_ || fail_)
-  {
-    fail_ = true;
-    return Debtor();
-  }
-  Debtor d;
-  d.name(readName());
-  d.debt(readValue());
-  if (d.name() == "" || (fail_ && valueExpected))
-  {
-    fail_ = true;
-    return Debtor();
-  }
-  clear();
-  checkEof();
-  return d;
-}
-
-void TokenStream::skip()
-{
-  ++it_;
-  checkEof();
 }
 
 TokenStream::operator bool()
 {
-  return !eof_ && !fail_;
+  return !(eof() || eol());
 }
 
-inline void TokenStream::checkEof()
+TokenStream &TokenStream::operator>>(CommandIO cmd)
 {
-  eof_ = (it_ == args_.end());
+  if (std::ws(in_).eof())
+    return *this;
+  std::string tempCommand;
+  in_ >> tempCommand;
+  if (in_.fail())
+    return *this;
+  if (tempCommand[0] == '/')
+    tempCommand = tempCommand.substr(1);
+  cmd.command = std::move(tempCommand);
+  return *this;
+}
+
+TokenStream &TokenStream::operator>>(OperationIO &opIO)
+{
+  using del = DelimiterNoExtractIO;
+  skipWs(in_);
+  std::istream::sentry s(in_);
+  if (!s || in_.eof() || in_.peek() == '\n')
+  {
+    in_.setstate(std::ios::failbit);
+    return *this;
+  }
+  OperationIO tOpIO;
+  in_ >> del{ '{' };
+  bool isRead = false;
+  readID(tOpIO.id, isRead);
+  if (isRead)
+  {
+    tOpIO.readFields = tOpIO.readFields | tOpIO.ID;
+    in_ >> del{ ',' };
+  }
+  readString(tOpIO.name, isRead);
+  if (isRead)
+  {
+    tOpIO.readFields = tOpIO.readFields | tOpIO.NAME;
+    in_ >> del{ ',' };
+  }
+  if (!(tOpIO.readFields & (DebtorIO::ID | DebtorIO::NAME)))
+  {
+    in_.setstate(std::ios::failbit);
+    return *this;
+  }
+  readInt(tOpIO.valueChange, isRead);
+  if (!isRead)
+  {
+    in_.setstate(std::ios::failbit);
+    return *this;
+  }
+  tOpIO.readFields = tOpIO.readFields | tOpIO.VALUE;
+  if (in_.peek() == ',')
+    in_.ignore(1);
+  readString(tOpIO.description, isRead);
+  if (isRead)
+    tOpIO.readFields = tOpIO.readFields | tOpIO.DECSRIPTION;
+  if (skipWs(in_).peek() != '}')
+  {
+    in_.setstate(std::ios::failbit);
+    return *this;
+  }
+  in_.ignore(1);
+  if (in_)
+    opIO = std::move(tOpIO);
+  return *this;
+}
+
+TokenStream &TokenStream::operator>>(DebtorIO &dIO)
+{
+  using del = DelimiterNoExtractIO;
+  skipWs(in_);
+  std::istream::sentry s(in_);
+  if (!s || in_.eof() || in_.peek() == '\n')
+  {
+    in_.setstate(std::ios::failbit);
+    return *this;
+  }
+  DebtorIO tDebtorIO;
+  in_ >> del{ '{' };
+  unsigned int id;
+  bool isRead = false;
+  readID(id, isRead);
+  if (isRead)
+  {
+    tDebtorIO.debtor.id(id);
+    tDebtorIO.readFields = tDebtorIO.readFields | tDebtorIO.ID;
+  }
+  if (in_.peek() == ',')
+    in_ >> del{ ',' }
+  ;
+  std::string name;
+  readString(name, isRead);
+  if (isRead)
+  {
+    tDebtorIO.debtor.name(name);
+    tDebtorIO.readFields = tDebtorIO.readFields | tDebtorIO.NAME;
+  }
+  if (!(tDebtorIO.readFields & (DebtorIO::ID | DebtorIO::NAME)))
+  {
+    in_.setstate(std::ios::failbit);
+    return *this;
+  }
+  if (in_.peek() == ',')
+  {
+    in_.ignore(1);
+    int debt;
+    readInt(debt, isRead);
+    if (isRead)
+    {
+      tDebtorIO.debtor.debt(debt);
+      tDebtorIO.readFields = tDebtorIO.readFields | tDebtorIO.DEBT;
+    }
+  }
+  if (skipWs(in_).peek() != '}')
+  {
+    in_.setstate(std::ios::failbit);
+    return *this;
+  }
+  in_.ignore(1);
+  if (in_)
+    dIO = tDebtorIO;
+  return *this;
+}
+
+void TokenStream::skipRestLine()
+{
+  while (!in_.eof() && in_.peek() != '\n')
+    in_.ignore(1);
+  in_.ignore(1);
+}
+
+bool TokenStream::eol()
+{
+  skipWs(in_);
+  return in_.peek() == '\n';
+}
+
+void TokenStream::readString(std::string &name, bool &readString)
+{
+  readString = false;
+  std::string tName;
+  skipWs(in_);
+  if (in_.peek() == '"')
+  {
+    in_.ignore(1);
+    char ch;
+    while (!in_.eof() && in_ && in_.peek() != '"' && in_.peek() != '\n')
+    {
+      in_ >> ch;
+      tName.push_back(ch);
+    }
+    if (!in_ || in_.eof() || in_.peek() == '\n')
+    {
+      in_.setstate(std::ios::failbit);
+      return;
+    }
+    in_.ignore(1);
+    name = tName;
+    readString = true;
+  }
+}
+
+void TokenStream::readID(unsigned int &id, bool &readID)
+{
+  readID = false;
+  unsigned int tId;
+  skipWs(in_);
+  if (isdigit(in_.peek()))
+  {
+    in_ >> tId;
+    if (!in_)
+    {
+      in_.setstate(std::ios::failbit);
+      return;
+    }
+    id = tId;
+    readID = true;
+  }
+}
+
+void TokenStream::readInt(int &value, bool &readInt)
+{
+  readInt = false;
+  int tValue;
+  skipWs(in_);
+  if (isdigit(in_.peek()) || in_.peek() == '-')
+  {
+    in_ >> tValue;
+    if (!in_)
+    {
+      return;
+    }
+    value = tValue;
+    readInt = true;
+  }
 }
--- token_stream.hpp
+++ token_stream.hpp
@@ -2,28 +2,77 @@
 #define TOKEN_STREAM_HPP
 #include <vector>
 #include <string>
+#include <iostream>
+#include "ioctl.hpp"
 #include "db_types/debtor.hpp"
 
+/**
+ * @ingroup ioctl
+ * @brief This class is used to extract IO structs from stream.
+ * @details It is much more comfortable to have class TokenStream
+ *    to control input. Mostly used in commandsIO.cpp file.
+ */
 class TokenStream
 {
 public:
-  TokenStream(const std::vector< std::string > &args);
-  TokenStream(const std::vector< std::string > &args, std::vector< std::string >::const_iterator it);
-  std::string readName();
-  int readValue();
-  Debtor readDebtor(bool valueExpected = false);
-  void skip();
-  std::vector< std::string >::const_iterator base() { return it_; }
-  bool eof() { return eof_; }
-  bool fail() { return fail_; }
-  void clear() { fail_ = false; }
+/**
+ * @brief Construct a new Token Stream object
+ * 
+ * @param[in] in istream with commands and arguments.
+ */
+  TokenStream(std::istream &in) : in_(in) {}
+  TokenStream(const TokenStream &) = delete;
+  TokenStream(TokenStream &&) = delete;
+  /**
+   * @brief This one reads Debtor from stream in format
+   *  {id, "name", debt}. One of id, name can be ommited. Debt also can be ommited. 
+   * 
+   * @param[in, out] d DebtorIO struct that will contain data. 
+   * @return Stream to continue work with it.
+   */
+  TokenStream &operator>>(DebtorIO &d);
+  /**
+   * @brief Reads command from stream. Returns eof and no fail if eof of stream was met.
+   * 
+   * @param[in, out] cmd CommandIO struct used to wrap std::string command.
+   * @return Stream to continue work with it.
+   */
+  TokenStream &operator>>(CommandIO cmd);
+  /**
+   * @brief This one reads Operation from stream in format
+   *  {id, "name", valueChange, "description"}. One of id, name can be ommited. Description also can be ommited. 
+   * 
+   * @param[in, out] opIO OperationIO struct that will contain data. 
+   * @return Stream to continue work with it.
+   */
+  TokenStream &operator>>(OperationIO &opIO);
+  /// @brief Skips line till newline symbol including or eof.
+  void skipRestLine();
+  /// @brief Checks if end of line was reached.
+  /// @return True if end of line was reached.
+  bool eol();
+  /// @brief Checks eof of given stream.
+  /// @return istream.eof()
+  inline bool eof() {
+    return in_.eof();
+  }
+  /// @brief Checks fail of given stream.
+  /// @return istream.fail()
+  inline bool fail() {
+    return in_.fail();
+  }
+  /// @brief Clears failbit of given stream.
+  inline void clear() {
+    in_.clear(in_.rdstate() & ~std::ios::failbit);
+  }
+  /// @brief Checks if there anything to read on current line.
+  /// @return !(eof() || eol());
   operator bool();
 private:
-  const std::vector< std::string > &args_;
-  std::vector< std::string >::const_iterator it_;
-  bool fail_ = false;
-  bool eof_ = false;
-  inline void checkEof();
+  std::istream &in_;
+  void readID(unsigned int &id, bool &readID);
+  void readString(std::string &name, bool &readString);
+  void readInt(int &value, bool &readInt);
 };
 
 #endif
