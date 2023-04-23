7194ac7e39fb1b015c4c8176b2ad56513ef2d830
Александр Абраамян <samvelcc25@outlook.com>
2023-04-23 12:37:29 +0300

    fix: fictive parametr was removed from constructor

--- db_types/operation.cpp
+++ db_types/operation.cpp
@@ -1,6 +1,6 @@
 #include "operation.hpp"
 
-Operation::Operation(std::shared_ptr< Debtor > d, int valueChange, long long int, std::string description) :
+Operation::Operation(std::shared_ptr< Debtor > d, int valueChange, std::string description) :
   debtor_(d),
   valueChange_(valueChange),
   time_(time(nullptr)),
