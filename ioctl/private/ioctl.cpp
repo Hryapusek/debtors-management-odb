77f57460fb973daac1b06179a6b96d909c98afaa
Александр Абраамян <samvelcc25@outlook.com>
2023-05-01 22:50:45 +0300

    change: now ioctl only contains low level functions

--- ioctl/private/ioctl.cpp
+++ ioctl/private/ioctl.cpp
@@ -1,7 +1,6 @@
 #include "ioctl.hpp"
 
 #include <iostream>
-#include "db_api.hpp"
 
 std::istream &operator>>(std::istream &in, DelimiterIO &&dest)
 {
@@ -36,44 +35,4 @@ std::istream &operator>>(std::istream &in, DelimiterNoExtractIO &&dest)
     in.ignore(1);
     return in;
   }
-}
-
-boost::optional< Debtor > helpersIO::getDebtor(DebtorIO &dIO)
-{
-  if (dIO.readFields & dIO.ID)
-    return api::getDebtor(dIO->id());
-  else
-    return api::getDebtor(dIO->name());
-}
-
-void helpersIO::addValue(OperationIO &opIO)
-{
-  if (opIO.readFields & opIO.NAME)
-    api::addValue(opIO.name, opIO.valueChange, opIO.description);
-  else if (opIO.readFields & opIO.ID)
-    api::addValue(opIO.id, opIO.valueChange, opIO.description);
-}
-
-void helpersIO::removeDebtor(DebtorIO &dIO)
-{
-  if (dIO.readFields & dIO.NAME)
-    api::removeDebtor(dIO->name());
-  else if (dIO.readFields & dIO.ID)
-    api::removeDebtor(dIO->id());
-}
-
-void helpersIO::rollback(DebtorIO &dIO)
-{
-  if (dIO.readFields & dIO.NAME)
-    api::rollback(dIO->name());
-  else if (dIO.readFields & dIO.ID)
-    api::rollback(dIO->id());
-}
-
-std::vector< Operation > helpersIO::getOperations(DebtorIO &dIO)
-{
-  if (dIO.readFields & dIO.NAME)
-    return api::getOperations(dIO->name());
-  else
-    return api::getOperations(dIO->id());
 }
